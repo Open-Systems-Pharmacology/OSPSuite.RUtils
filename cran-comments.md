04ae46478dfc30715df53d386a51f6d4cd011a2d
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-03-30 14:00:13 +0200

    Adds `validateIsFileExtension()`
    Closes #92
    
    Prepare for CRAN submission

--- cran-comments.md
+++ cran-comments.md
@@ -1,6 +1,11 @@
+## Test environments
+
+* local R installation, R 4.1.3
+* ubuntu 16.04 (on github-actions), R 4.1.3
+* win-builder (devel)
+
 ## R CMD check results
 
 0 errors | 0 warnings | 0 note
 
-* Hotfix release to fix a detected regression.
-
+* Adds new functions.
--- tests/testthat/test-error-checks.R
+++ tests/testthat/test-error-checks.R
@@ -17,35 +17,35 @@ z <- 3
 
 a <- 1
 
-test_that("isSameLength returns TRUE when objects have same lengths", {
+test_that("isSameLength returns `TRUE` when objects have same lengths", {
   expect_true(isSameLength(A, A))
   expect_true(isSameLength(c(1, 2), c("x", "y"), c(TRUE, FALSE)))
   expect_true(isSameLength(list(1, 2), list("x", "y"), list(TRUE, FALSE)))
   expect_true(isSameLength(character(), integer()))
 })
 
-test_that("isSameLength returns FALSE when objects have different lengths", {
+test_that("isSameLength returns `FALSE` when objects have different lengths", {
   expect_false(isSameLength(A, B))
   expect_false(isSameLength(c(1, 2), c("x"), c(TRUE, FALSE)))
   expect_false(isSameLength(list(1, 2), list("x", "y"), list(FALSE)))
 })
 
 
-test_that("isOfLength returns TRUE when correct length is specified", {
+test_that("isOfLength returns `TRUE` when correct length is specified", {
   expect_true(isOfLength(A, 3L))
   expect_true(isOfLength(c(1, 2), 2L))
   expect_true(isOfLength(list("1", 2, 4.5, TRUE), 4L))
   expect_true(isOfLength(character(), 0L))
 })
 
-test_that("isOfLength returns FALSE when incorrect length is specified", {
+test_that("isOfLength returns `FALSE` when incorrect length is specified", {
   expect_false(isOfLength(A, 5))
   expect_false(isOfLength(c(1, 2), 3L))
   expect_false(isOfLength(list("1", 2, 4.5, TRUE), 3L))
   expect_false(isOfLength(character(), 1L))
 })
 
-test_that("isIncluded returns TRUE when base type values are included", {
+test_that("isIncluded returns `TRUE` when base type values are included", {
   expect_true(isIncluded("col3", names(A)))
   expect_true(isIncluded(2, 2))
   expect_true(isIncluded("x", list("w", "x", 1, 2)))
@@ -55,7 +55,7 @@ test_that("isIncluded returns TRUE when base type values are included", {
   expect_true(isIncluded(a, c(x, y, z)))
 })
 
-test_that("isIncluded returns TRUE when compound type values are included", {
+test_that("isIncluded returns `TRUE` when compound type values are included", {
   skip_if_not(getRversion() > "4.1")
 
   expect_true(isIncluded(as.factor("a"), c("a", "b")))
@@ -66,7 +66,7 @@ test_that("isIncluded returns TRUE when compound type values are included", {
   expect_true(isIncluded(as.Date("1970-02-01"), list(as.Date("1970-02-01"), as.Date("1980-12-21"))))
 })
 
-test_that("isIncluded returns FALSE when base type values are not included", {
+test_that("isIncluded returns `FALSE` when base type values are not included", {
   expect_false(isIncluded("col4", names(A)))
   expect_false(isIncluded(1, 2))
   expect_false(isIncluded("x", c("w", "a", "y")))
@@ -78,7 +78,7 @@ test_that("isIncluded returns FALSE when base type values are not included", {
   expect_false(isIncluded(character()))
 })
 
-test_that("isIncluded returns FALSE when compound type values are not included", {
+test_that("isIncluded returns `FALSE` when compound type values are not included", {
   skip_if_not(getRversion() > "4.1")
 
   expect_false(isIncluded(as.factor("a"), c("d", "b")))
@@ -106,7 +106,7 @@ test_that("isOfType doesn't work when `nullAllowed` argument is not logical", {
   )
 })
 
-test_that("isOfType returns TRUE when values are of expected type", {
+test_that("isOfType returns `TRUE` when values are of expected type", {
   expect_true(isOfType(A, "data.frame"))
   expect_true(isOfType(list(A, B), "data.frame"))
   expect_true(isOfType(c(1, "x"), c("numeric", "character")))
@@ -125,11 +125,11 @@ test_that("isOfType returns TRUE when values are of expected type", {
   expect_true(isOfType(tmp, Person, nullAllowed = TRUE))
 })
 
-test_that("isOfType returns FALSE when values are not of expected type", {
+test_that("isOfType returns `FALSE` when values are not of expected type", {
   expect_false(isOfType(A, "character"))
 })
 
-test_that("isEmpty returns TRUE when objects are empty", {
+test_that("isEmpty returns `TRUE` when objects are empty", {
   expect_true(isEmpty(NULL))
   expect_true(isEmpty(data.frame()))
   expect_true(isEmpty(list()))
@@ -138,14 +138,14 @@ test_that("isEmpty returns TRUE when objects are empty", {
   expect_true(isEmpty(A[FALSE, ]))
 })
 
-test_that("isEmpty returns FALSE when objects are not empty", {
+test_that("isEmpty returns `FALSE` when objects are not empty", {
   expect_false(isEmpty(a))
   expect_false(isEmpty(A))
   expect_false(isEmpty(list("x", "y")))
   expect_false(isEmpty(""))
 })
 
-test_that("hasEmptyString returns TRUE when strings are empty", {
+test_that("hasEmptyString returns `TRUE` when strings are empty", {
   expect_true(hasEmptyString(character(0)))
   expect_true(hasEmptyString(c("", "y")))
   expect_true(hasEmptyString(list("", "y")))
@@ -156,27 +156,52 @@ test_that("hasEmptyString returns TRUE when strings are empty", {
   expect_true(hasEmptyString(list(NA, "x", "y")))
 })
 
-test_that("hasEmptyString returns FALSE when string are not empty", {
+test_that("hasEmptyString returns `FALSE` when string are not empty", {
   expect_false(hasEmptyString("   abc   "))
   expect_false(hasEmptyString(c("x", "y")))
   expect_false(hasEmptyString(list("x", "y")))
 })
 
-test_that("hasOnlyDistinctValues returns TRUE if values are distinct", {
+test_that("hasOnlyDistinctValues returns `TRUE` if values are distinct", {
   expect_true(hasOnlyDistinctValues(c("x", NA, "y")))
   expect_true(hasOnlyDistinctValues(list("x", NA, "y")))
 })
 
-test_that("hasOnlyDistinctValues returns FALSE if values are repeated", {
+test_that("hasOnlyDistinctValues returns `FALSE` if values are repeated", {
   expect_false(hasOnlyDistinctValues(c("x", NA, "y", "x")))
   expect_false(hasOnlyDistinctValues(list("x", NA, "y", "x")))
 })
 
-test_that("isFileExtension correctly detects if extension is as expected", {
+test_that("isFileExtension returns `TRUE` if extension is as expected", {
   expect_true(isFileExtension("enum.R", "R"))
   expect_true(isFileExtension("DESCRIPTION", ""))
   expect_true(isFileExtension("foo.", ""))
   expect_true(isFileExtension("C:/Users/.gitignore", "gitignore"))
+})
 
+test_that("isFileExtension returns `FALSE` if extension is unexpected", {
   expect_false(isFileExtension("enum.R", "pkml"))
+  expect_false(isFileExtension("DESCRIPTION", "R"))
+})
+
+
+test_that("validateIsFileExtension returns NULL if extension is as expected", {
+  expect_null(validateIsFileExtension("enum.R", "R"))
+  expect_null(validateIsFileExtension("DESCRIPTION", ""))
+  expect_null(validateIsFileExtension("foo.", ""))
+  expect_null(validateIsFileExtension("C:/Users/.gitignore", "gitignore"))
+})
+
+test_that("validateIsFileExtension produces error if extension is unexpected", {
+  expect_error(
+    validateIsFileExtension("enum.R", "pkml"),
+    messages$errorWrongFileExtension("R", "pkml"),
+    fixed = TRUE
+  )
+
+  expect_error(
+    validateIsFileExtension("DESCRIPTION", "R"),
+    messages$errorWrongFileExtension("", "R"),
+    fixed = TRUE
+  )
 })
